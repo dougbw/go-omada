8b4afe2a15e468b353cafa87ed1024e0be6ff32c
dougbw <dbw>
2023-07-07 21:34:00 +0100

    fixing

--- login.go
+++ login.go
@@ -18,7 +18,7 @@ type Controller struct {
 	controllerId string
 	token        string
 	siteId       string
-	Sites        map[string]string
+	sites        map[string]string
 }
 
 type ControllerInfo struct {
@@ -170,46 +170,9 @@ func (c *Controller) Login(user string, pass string) error {
 	token := login.Result.Token
 	c.token = token
 
-	err = c.getSites()
+	err = c.GetSites()
 	if err != nil {
 		return err
-	}
-
-	return nil
-
-}
-
-func (c *Controller) getSites() error {
-
-	path := "api/v2/users/current"
-	url := fmt.Sprintf("%s/%s/%s", c.baseURL, c.controllerId, path)
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Accept", "application/json")
-	req.Header.Add("Csrf-Token", c.token)
-
-	res, err := c.httpClient.Do(req)
-	if err != nil {
-		return err
-	}
-
-	if res.StatusCode != http.StatusOK {
-		err = fmt.Errorf("status code: %d", res.StatusCode)
-		return err
-	}
-
-	var currentUserResponse currentUserResponse
-	if err := json.NewDecoder(res.Body).Decode(&currentUserResponse); err != nil {
-		return err
-	}
-
-	c.sites = make(map[string]string)
-	for _, v := range currentUserResponse.Result.Privilege.Sites {
-		c.sites[v.Name] = v.Key
-		c.SetSite(v.Name)
 	}
 
 	return nil
--- site.go
+++ site.go
@@ -1,6 +1,47 @@
 package omada
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func (c *Controller) GetSites() error {
+
+	path := "api/v2/users/current"
+	url := fmt.Sprintf("%s/%s/%s", c.baseURL, c.controllerId, path)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Accept", "application/json")
+	req.Header.Add("Csrf-Token", c.token)
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		err = fmt.Errorf("status code: %d", res.StatusCode)
+		return err
+	}
+
+	var currentUserResponse currentUserResponse
+	if err := json.NewDecoder(res.Body).Decode(&currentUserResponse); err != nil {
+		return err
+	}
+
+	c.sites = make(map[string]string)
+	for _, v := range currentUserResponse.Result.Privilege.Sites {
+		c.sites[v.Name] = v.Key
+		c.SetSite(v.Name)
+	}
+
+	return nil
+
+}
 
 func (c *Controller) SetSite(site string) error {
 	siteId, ok := c.sites[site]

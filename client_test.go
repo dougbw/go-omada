f7251e7ed860a306f2d5119557e8cf774bac05e1
dougbw <douglas.brand-williamson@underwriteme.co.uk>
2024-01-19 17:38:54 +0000

    work in progress adding tests

--- client_test.go
+++ client_test.go
@@ -12,84 +12,66 @@ import (
 
 func TestGetClients(t *testing.T) {
 
-	controllerId := "978bee230c77bbb45d9c8545d04d700a"
+	controllerId := "123bee230c77bbb45d9c8545d04d700a"
 	siteId := "Default"
-
 	pathLogin := fmt.Sprintf("/%s/api/v2/login", controllerId)
 	pathUsers := fmt.Sprintf("/%s/api/v2/users/current", controllerId)
 	pathClients := fmt.Sprintf("/%s/api/v2/sites/%s/clients", controllerId, siteId)
+	pathDevices := fmt.Sprintf("/%s/api/v2/sites/%s/devices", controllerId, siteId)
+	pathNetworks := fmt.Sprintf("/%s/api/v2/sites/%s/setting/lan/networks", controllerId, siteId)
 
-	responseMap := map[string]string{
-		"/api/info": "./test-data/info-response.json",
-		pathLogin:   "./test-data/info-response.json",
-		pathUsers:   "./test-data/users-response.json",
-		pathClients: "./test-data/client-response.json",
+	responses := map[string]string{
+		"/api/info":  "./test-data/info-response.json",
+		pathLogin:    "./test-data/info-response.json",
+		pathUsers:    "./test-data/users-response.json",
+		pathClients:  "./test-data/clients-response.json",
+		pathDevices:  "./test-data/devices-response.json",
+		pathNetworks: "./test-data/networks-response.json",
 	}
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		switch r.URL.Path {
-		case "/api/info":
-			response, err := os.ReadFile("./test-data/info-response.json")
-			if err != nil {
-				fmt.Print(err)
-			}
-			w.WriteHeader(http.StatusOK)
-			w.Write(response)
-		case pathLogin:
-			response, err := os.ReadFile("./test-data/login-response.json")
-			if err != nil {
-				fmt.Print(err)
-			}
-			w.WriteHeader(http.StatusOK)
-			w.Write(response)
-		case pathUsers:
-			response, err := os.ReadFile("./test-data/users-response.json")
-			if err != nil {
-				fmt.Print(err)
-			}
-			w.WriteHeader(http.StatusOK)
-			w.Write(response)
-
-		case pathClients:
-			response, err := os.ReadFile("./test-data/client-response.json")
-			if err != nil {
-				fmt.Print(err)
-			}
-			w.WriteHeader(http.StatusOK)
-			w.Write(response)
-		default:
+		responseFile, ok := responses[r.URL.Path]
+		if !ok {
 			t.Errorf("Unexpected request path on mock server: %s", r.URL.Path)
 		}
-
+		response, err := os.ReadFile(responseFile)
+		if err != nil {
+			fmt.Print(err)
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
 	}))
 	defer server.Close()
 
 	controller := New(server.URL)
 	err := controller.GetControllerInfo()
 	if err != nil {
-		t.Fatalf("test failure: get controller info")
+		t.Fatalf("test failure on 'GetControllerInfo': %v", err)
 	}
 	err = controller.Login("user", "pass")
 	if err != nil {
-		t.Fatalf("test failure: controller login: %v", controller)
+		t.Fatalf("test failure on 'Login': %v", err)
 	}
 
 	err = controller.SetSite("Home")
 	if err != nil {
-		t.Fatalf("test failure: set site: %v", err)
+		t.Fatalf("test failure on 'SetSite': %v", err)
 	}
 
 	clients, err := controller.GetClients()
 	if err != nil {
-		t.Fatalf("test failure: get clients")
+		t.Fatalf("test failure on 'GetClients': %v", err)
 	}
 
-	// assert
+	assert.Equal(t, controller.siteId, "Default")
 	expectedClients := 3
 	assert.Len(t, clients, expectedClients)
-	assert.Equal(t, "coredns 123", clients[0].Name)
-	assert.Equal(t, "10.0.0.5", clients[0].Ip)
-	assert.Equal(t, "coredns-123", clients[0].DnsName)
+	assert.Equal(t, "Client 001", clients[0].Name)
+	assert.Equal(t, "10.0.0.101", clients[0].Ip)
+	assert.Equal(t, "client-001", clients[0].DnsName)
+
+	assert.Equal(t, "Google Nest Mini", clients[1].Name)
+	assert.Equal(t, "10.0.0.103", clients[1].Ip)
+	assert.Equal(t, "google-nest-mini", clients[1].DnsName)
 
 }

af5e41aa9e5485dd1418ad748b901dc48c8d075e
Dan Cech <dcech@grafana.com>
2024-04-06 11:44:06 -0400

    add more client properties

--- client_test.go
+++ client_test.go
@@ -17,11 +17,17 @@ func TestGetClients(t *testing.T) {
 	expectedClients := 3
 	assert.Equal(t, testController.siteId, "Default")
 	assert.Len(t, clients, expectedClients)
-	assert.Equal(t, "Client 001", clients[0].Name)
-	assert.Equal(t, "Google Nest Mini", clients[1].Name)
-	assert.Equal(t, "client-001", clients[0].DnsName)
-	assert.Equal(t, "google-nest-mini", clients[1].DnsName)
 
+	assert.Equal(t, "Client 001", clients[0].Name)
+	assert.Equal(t, "client-001", clients[0].DnsName)
 	ip := net.ParseIP(clients[0].Ip) // ParseIP returns nil rather than error if unable to parse
 	assert.NotNil(t, ip)
+
+	assert.Equal(t, "Google Nest Mini", clients[1].Name)
+	assert.Equal(t, "google-nest-mini", clients[1].DnsName)
+
+	assert.Equal(t, "win10-vm", clients[2].Name)
+	assert.Equal(t, "win10-vm", clients[2].DnsName)
+	assert.Equal(t, 1, len(clients[2].IPV6List))
+	assert.Equal(t, "ffff::ffff:ffff:ffff:ffff", clients[2].IPV6List[0])
 }
